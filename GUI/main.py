aa6491e29eb340f3dc6aa71dd27b9ff6c785aa97
ema902 <ema.farshi@mail.utoronto.ca>
2026-03-24 12:36:35 -0400

    fixed all the bugs! basic skeleton of program is done. You can now switch screens (i havent added back to main menu buttons yet but its a quick fix!!)

--- GUI/main.py
+++ GUI/main.py
@@ -15,14 +15,16 @@ algorithm_screen = screen_displays.Screen([], algorithm_image_file_path, screen)
 search_image_screen = screen_displays.Screen([], search_image_file_path, screen)
 current_screen = screen_displays.ScreenOrganizer(main_menu_screen)
 # Main menu buttons
-pa_rect = pygame.Rect(500, 500, 100, 60)
+pa_rect = pygame.Rect(500, 500, 200, 60)
 pa_color = (148, 124, 92)
 pa_button = screen_displays.Button(pa_rect, "Survey", pa_color,
-                                   lambda: screen_displays.proceed_to_algorithm(algorithm_screen, current_screen))
-si_rect = pygame.Rect(500, 700, 100, 60)
+                                   lambda: screen_displays.proceed_to_algorithm(algorithm_screen, current_screen),
+                                   (400, 540))
+si_rect = pygame.Rect(500, 600, 200, 60)
 si_color = (148, 124, 92)
-si_button = screen_displays.Button(pa_rect, "Survey", pa_color,
-                                   lambda: screen_displays.proceed_to_graph(search_image_screen, current_screen))
+si_button = screen_displays.Button(si_rect, "Search", pa_color,
+                                   lambda: screen_displays.proceed_to_graph(search_image_screen,
+                                                                            current_screen), (400, 640))
 
 # Main menu screen
 main_menu_screen.buttons = [pa_button, si_button]
@@ -33,9 +35,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             break
-        current_screen.screen.update_all_buttons(event)
-    current_screen.screen.draw_screen()
-
+        if current_screen.curr_screen == algorithm_screen:
+            algorithm_screen.draw_screen()
+        if current_screen.curr_screen.screen == search_image_screen:
+            search_image_screen.draw_screen()
+        if current_screen.curr_screen.screen == main_menu_screen:
+            main_menu_screen.draw_screen()
+        current_screen.curr_screen.update_all_buttons(event)
+    current_screen.curr_screen.draw_screen()
     pygame.display.flip()
+
 pygame.quit()
 sys.exit()
--- GUI/screen_displays.py
+++ GUI/screen_displays.py
@@ -16,19 +16,22 @@ class Button:
     text: str
     color: tuple[int, int, int]
     action: Callable[..., None]
+    top_left_coordinates: tuple[int, int]
 
-    def __init__(self, rect : pygame.Rect, text: str, color: tuple[int, int, int],
-                 action: Callable[..., None]):
+    def __init__(self, rect: pygame.Rect, text: str, color: tuple[int, int, int],
+                 action: Callable[..., None], top_left_coordinates: tuple[int, int]):
         self.rect = rect
         self.font = pygame.font.SysFont("candara", 30)
         self.text = text
         self.color = color
         self.action = action
+        self.top_left_coordinates = top_left_coordinates
 
     def draw_button(self, surface: pygame.Surface) -> None:
         """Draws the given button on given surface.
         """
         BLACK = (0, 0, 0)
+        self.rect.topleft = self.top_left_coordinates
         pygame.draw.rect(surface, self.color, self.rect)
         text_surf = self.font.render(self.text, True, BLACK)
         text_rect = text_surf.get_rect(center=self.rect.center)
@@ -37,9 +40,8 @@ class Button:
     def perform_event(self, event: pygame.event.Event) -> None:
         """Checks over if event was a click, if click collided with button surface area then perform action
         """
-        if self.is_clicked(event):
-            print("action performed")
-            self.action()
+        print("action performed")
+        self.action()
 
     def is_clicked(self, event: pygame.event.Event) -> bool:
         """ Returns whether the given event is the user clicking the button.
@@ -68,14 +70,13 @@ class Screen:
     image_filepath: str
 
     def __init__(self, buttons: list[Button], image_filepath: str, screen: pygame.Surface):
+        self.screen = screen
         self.WIDTH = 1000
         self.HEIGHT = 1000
         self.buttons = buttons
         self.image_filepath = image_filepath
         self.image = pygame.image.load(self.image_filepath)
         self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGHT))
-        self.screen = screen
-
 
     def update_all_buttons(self, event: pygame.event.Event) -> None:
         """Checks if all buttons are clicked in self."""
@@ -86,7 +87,7 @@ class Screen:
     def draw_screen(self) -> None:
         """Draws the screen in pygame with buttons.
         """
-        self.screen.blit(self.image, (0,0))
+        self.screen.blit(self.image, (0, 0))
         for button in self.buttons:
             button.draw_button(self.screen)
         pygame.display.flip()
@@ -96,11 +97,9 @@ class ScreenOrganizer:
     """Manages the current Screen on pygame and updates according to button"""
     curr_screen: Screen
 
-    def __init__(self, screen: Screen = None):
-        if screen is None:
-            self.screen = None  # set to default screen
-        else:
-            self.screen = screen
+    def __init__(self, screen: Screen):
+        # set to default screen
+        self.curr_screen = screen
 
     def switch_screens(self, new_screen: Screen) -> None:
         """Swaps the screen with new_screen and makes it curr_screen"""
@@ -108,8 +107,12 @@ class ScreenOrganizer:
 
 
 def proceed_to_graph(search_screen: Screen, current_screen: ScreenOrganizer) -> None:
+    """Switches from current screen to search screen
+    """
     current_screen.switch_screens(search_screen)
 
 
 def proceed_to_algorithm(algorithm_screen: Screen, current_screen: ScreenOrganizer) -> None:
+    """Switches from current screen to algorithm screen
+    """
     current_screen.switch_screens(algorithm_screen)
